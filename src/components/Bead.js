bacaf9bb42abf1155e4ea6e6084a359879e89c8c
VanFanelia <van@gmx-topmail.de>
2017-04-16 18:44:21 +0200

    Makes Bead.js as a drop target with empty functions

--- src/components/Bead.js
+++ src/components/Bead.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import classNames from 'classnames';
+import { DropTarget } from 'react-dnd';
+
 import injectSheet from '../utils/injectSheet';
+import blankBead from '../graphics/BlankBead.svg';
+import ItemTypes from '../utils/dnd/ItemTypes';
 import grid from '../utils/grid';
+
 import { jssSheet } from '../utils/propTypes';
 import { transparent } from '../style/colors';
-import blankBead from '../graphics/BlankBead.svg';
+
+
+const beadTarget = {
+	drop(props, monitor) {
+		console.log(props);
+		console.log(monitor.getItem());
+		// setBead();
+	},
+};
+
+function collect(connect, monitor) {
+	return {
+		connectDropTarget: connect.dropTarget(),
+		isOver: monitor.isOver(),
+	};
+}
 
 const styles = {
 	bead: {
+		position: 'relative',
 		height: grid('xs'),
 		width: grid('xs'),
 		lineHeight: grid('xs'),
@@ -29,15 +50,30 @@ const styles = {
 };
 
 const Bead = ({ sheet: { classes }, ...props }) => (
-	<span
-		id={props.beadId}
-		className={classNames({
-			[classes.bead]: true,
-			[classes.empty]: props.empty },
-		)}
-	>
-		{props.empty ? (<img className={classes.icon} src={blankBead} alt="" />) : 'foo'}
-	</span>
+	props.connectDropTarget(
+		<span
+			id={props.beadId}
+			className={classNames({
+				[classes.bead]: true,
+				[classes.empty]: props.empty },
+			)}
+		>
+			{props.empty ? (<img className={classes.icon} src={blankBead} alt="" />) : 'foo'}
+			{props.isOver &&
+				<div
+					style={{
+						position: 'absolute',
+						top: 0,
+						left: 0,
+						height: '100%',
+						width: '100%',
+						zIndex: 1,
+						opacity: 0.5,
+						backgroundColor: 'green',
+					}}
+				/>
+			}
+		</span>)
 );
 
 Bead.propTypes = {
@@ -47,10 +83,11 @@ Bead.propTypes = {
 	empty: React.PropTypes.bool,
 	bead: React.PropTypes.func,
 	sheet: jssSheet,
+	isOver: React.PropTypes.bool.isRequired,
 };
 
 Bead.defaultProps = {
 	empty: true,
 };
 
-export default injectSheet(styles)(Bead);
+export default DropTarget(ItemTypes.BEAD, beadTarget, collect)(injectSheet(styles)(Bead));
